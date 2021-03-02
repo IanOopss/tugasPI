e5693ddde0e326358f28da46aa116e03710a74e1
IanOopss <56650504+IanOopss@users.noreply.github.com>
2021-03-02 15:01:24 +0700

    Menerima Inputan

---
+++ menerimainputan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+float x, y, hsl;
+cout << "Angka Pertama = " ;
+cin >> x;
+cout << "Angka Kedua = " ;
+cin >> y;
+hasil = x + y;
+cout << "Hasil      = " << hsl;
+
+return 0;
+}
