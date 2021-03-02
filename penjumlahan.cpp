ee2ea8b2d09c06292001478efd6dc978bbf6da2a
IanOopss <56650504+IanOopss@users.noreply.github.com>
2021-03-02 15:02:15 +0700

    Update penjumlahan.cpp

--- penjumlahan.cpp
+++ penjumlahan.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(){
 
-int x, y, hsl;
-
-x = 8;
-y = 2;
-
-hsl = x + y;
-cout << "Hasil  = " << hsl;
+float x, y, hsl;
+cout << "Angka Pertama = " ;
+cin >> x;
+cout << "Angka Kedua = " ;
+cin >> y;
+hasil = x + y;
+cout << "Hasil      = " << hsl;
 
 return 0;
 }
